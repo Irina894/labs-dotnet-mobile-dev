bfae73c408308f820f3d58f3319d462ad6f28f83
Irina894 <irinaderevoriz@gmail.com>
2026-05-02 13:27:57 +0300

    Lab1 is done

---
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                CallBtn.IsEnabled = true;
+                CallBtn.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallBtn.IsEnabled = false;
+                CallBtn.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await DisplayAlert(
+                "Dial Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Number was not valid.", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+    }
+}
---
+++ Phoneword/PhonewordTranslator.cs
@@ -0,0 +1,40 @@
+﻿namespace Phoneword
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new System.Text.StringBuilder();
+            foreach (var c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c)) return 2;
+            if ("DEF".Contains(c)) return 3;
+            if ("GHI".Contains(c)) return 4;
+            if ("JKL".Contains(c)) return 5;
+            if ("MNO".Contains(c)) return 6;
+            if ("PQRS".Contains(c)) return 7;
+            if ("TUV".Contains(c)) return 8;
+            if ("WXYZ".Contains(c)) return 9;
+            return null;
+        }
+    }
+}
